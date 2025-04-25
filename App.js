9f3f11e2ba39f0920b40f1b2c660575cb30b0ef4
Juanivene <168385555+Juanivene@users.noreply.github.com>
2025-04-25 18:26:31 -0300

    feat: text, button,image, statusbar and touchableHighlight

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 
+const icon = require("./assets/icon.png");
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      {/* <Image
+        source={{
+          uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS01VRmI2QRTnH9LX26f2FbHXGYE-vt_PJWXg&s",
+        }}
+        style={{ width: 215, height: 294 }}
+      /> */}
+      {/* <Text style={{ color: "white", padding: 10 }}>
+        Las cadenas de teto siempre deben estar renderizadas en un componente
+        texto..
+      </Text> */}
+      {/* <Button
+        title="Pulsa aquí"
+        onPress={() => {
+          alert("Alerta de prueba ");
+        }}
+      /> */}
+      <TouchableHighlight
+        underlayColor="09f"
+        onPress={() => {
+          alert("Alerta de prueba");
+        }}
+      >
+        <Text style={{ color: "white" }}>Presione Aqui</Text>
+      </TouchableHighlight>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    color: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
